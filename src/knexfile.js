93d88ae0dd449100d72b5d140e1dd24856425fee
coder-shankar <34474567+coder-shankar@users.noreply.github.com>
2018-09-02 17:57:59 +0545

    Update knexfile.js

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,27 +1,19 @@
-require('babel-register');
-require('dotenv').config({ path: __dirname + '/../.env' });
+require("babel-register");
+require("dotenv").config({ path: __dirname + "/../.env" });
 
 /**
  * Database configuration.
  */
 module.exports = {
   client: process.env.DB_CLIENT,
-  connection: {
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.NODE_ENV === 'test' ? process.env.TEST_DB_NAME : process.env.DB_NAME,
-    charset: 'utf8',
-    timezone: 'UTC'
-  },
+  connection: process.env.DATABASE_URL,
   migrations: {
-    tableName: 'migrations',
-    directory: './migrations',
-    stub: './stubs/migration.stub'
+    tableName: "migrations",
+    directory: "./migrations",
+    stub: "./stubs/migration.stub"
   },
   seeds: {
-    directory: './seeds',
-    stub: './stubs/seed.stub'
+    directory: "./seeds",
+    stub: "./stubs/seed.stub"
   }
 };
